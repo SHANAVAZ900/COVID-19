81bfc546b2972bf9c81c850d07fcfc1de4501588
zamindar-786 <zamindar900@gmail.com>
2021-05-01 01:14:45 +0530

    created and done styling for table.js

--- covid-19/src/Table.js
+++ covid-19/src/Table.js
@@ -1,17 +1,18 @@
 import React from 'react'
+import "./Table.css";
 
 function Table({ countries }) {
     return (
         <div className="table">
             {countries.map(({ country, cases }) =>(
-                <tr>
-                <td>
-                    {country}
-                </td>
-                <td>
-                    <strong>{cases}</strong>
-                </td>
-            </tr>
+                <tr> 
+                    <td>
+                        {country}
+                    </td>
+                    <td>
+                        <strong>{cases}</strong>
+                    </td>
+                </tr>
             )
             )}
             
